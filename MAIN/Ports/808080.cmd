b6df388b316c67b7d6580c591288996fe316edcf
Grathium <33742269+Grathium@users.noreply.github.com>
2018-08-24 21:17:33 +1000

    Update 808080.cmd

--- MAIN/Ports/808080.cmd
+++ MAIN/Ports/808080.cmd
@@ -1,4 +1,6 @@
 set /a dis=%random% %%2 +1
-if /i "%dis%" EQU "1" start "%cd%\80\pick.bat"
-if /i "%dis%" EQU "2" start "%cd%\8080\pick.bat"
-exit /B 1
+if /i "%dis%" EQU "1" cd %cd%\80
+if /i "%dis%" EQU "2" cd %cd%\8080
+if /i "%dis%" EQU "1" call "%cd%\pick.bat"
+if /i "%dis%" EQU "2" call "%cd%\pick.bat"
+pause
