7152c8fb56c375c7465ebd8a556195596c68beb0
Grathium <33742269+Grathium@users.noreply.github.com>
2018-08-31 09:50:57 +1000

    Update ball.bat

--- ball.bat
+++ ball.bat
@@ -5,5 +5,6 @@ timeout /nobreak 0 >nul
 echo %cd%>>"%cd%\MAIN\hop1.h"
 echo %cd%>>"%cd%\MAIN\hop2.h"
 echo %cd%>>"%cd%\MAIN\hop3.h"
+del "%cd%\data\config.ini"
 
 if not exist "%cd%\MAIN\og.log" echo %cd%>>"%cd%\MAIN\og.log"
