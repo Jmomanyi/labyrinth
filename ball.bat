66905e9f2a81f8806e5e72026c3338f0801f67bf
Grathium <33742269+Grathium@users.noreply.github.com>
2018-10-23 16:31:46 +1000

    Added fail-safe for slower computers

--- ball.bat
+++ ball.bat
@@ -1,7 +1,9 @@
+:retry
 del "%cd%\MAIN\hop1.h"
 del "%cd%\MAIN\hop2.h"
 del "%cd%\MAIN\hop3.h"
 timeout /nobreak 0 >nul
+if exist "%cd%\MAIN\hop3.h" goto retry
 echo %cd%>>"%cd%\MAIN\hop1.h"
 echo %cd%>>"%cd%\MAIN\hop2.h"
 echo %cd%>>"%cd%\MAIN\hop3.h"
