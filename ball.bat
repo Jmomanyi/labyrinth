e42ffa10d257771b414e52bf09fa9506fdcf33fa
Grathium <33742269+Grathium@users.noreply.github.com>
2018-10-26 14:06:58 +1000

    Update ball.bat

--- ball.bat
+++ ball.bat
@@ -4,9 +4,9 @@ del "%cd%\MAIN\hop2.h"
 del "%cd%\MAIN\hop3.h"
 timeout /nobreak 0 >nul
 if exist "%cd%\MAIN\hop3.h" goto retry
-echo %cd%>>"%cd%\MAIN\hop1.h"
-echo %cd%>>"%cd%\MAIN\hop2.h"
-echo %cd%>>"%cd%\MAIN\hop3.h"
+echo %cd%\>>"%cd%\MAIN\hop1.h"
+echo %cd%\>>"%cd%\MAIN\hop2.h"
+echo %cd%\>>"%cd%\MAIN\hop3.h"
 del "%cd%\data\config.ini"
 
 if not exist "%cd%\MAIN\og.log" echo %cd%>>"%cd%\MAIN\og.log"
